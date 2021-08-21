3a838841251c103518aa5f6b554bedb8108e14de
Lawrence Darwin Reid <LawrenceDarwinReid@gmail.com>
2021-08-21 04:02:41 -0400

    modified the structure of pages, added JavaScript for the nav, Note:JavaScript link might have to be re-linked after pull request

---
+++ siteFiles/JavaScript/nav.js
@@ -0,0 +1,40 @@
+// This substitutes the empty navHTML class Tag with the inner html
+// This substitutes the empty navHTML class Tag with the inner html
+const nav = document.querySelector(".navHTML");
+
+const navBar = document.createElement("nav");
+
+navBar.innerHTML =
+  '<div class="navLinksToTheRight"> <a href="/index.html">Home</a> <a href="#contact">Contact</a> <a href="#about">About</a> </div>';
+nav.parentNode.replaceChild(navBar, nav);
+
+/* 
+
+Steps to modify the navBar and then use the join lines command 
+
+1. Highlight Lines, Open the Command Palette Ctrl + Shift + P 
+
+or:
+
+a) Top Left of screen
+
+b) View Menu
+
+c) Click Command Palette
+
+2. type Join Lines, then pres enter
+
+3. cut and paste line between the single quotes ''
+
+//////////////////////////////////////
+
+current nav structure:
+
+<div class="navLinksToTheRight">
+  <a href="/index.html">Home</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+ 
+
+*/
